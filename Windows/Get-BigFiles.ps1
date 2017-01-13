110f6dccfe3cbdd0bbc86ca2c990b90adefc7c49
Benny <benny_boy66@hotmail.fr>
2017-01-13 11:14:47 +0100

    Initial Release

---
+++ Windows/Get-BigFiles.ps1
@@ -0,0 +1,86 @@
+﻿#requires -version 3
+
+Function Get-BigFiles {
+<#
+    .SYNOPSIS
+            This function will scan a folder in order to retrieve the files which exceed a given 
+            size threshold
+    .DESCRIPTION
+            This function will scan a folder in order to retrieve the files which exceed a given 
+            size threshold
+    .PARAMETER  Path
+            Specifies the folder to scan recursively
+    .PARAMETER  MinSize
+            Specifies the minimal threshold
+    .EXAMPLE
+            Get-BigFiles -Path "D:\temp\vsphere6.1"
+
+            FullName                      Date Created             Date Modified            Size (MB)
+            --------                      ------------             -------------            ---------
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:07:33      20/05/2015 11:50:35         2613,7
+            D:\temp\vsphere6.1\vRealiz... 16/06/2015 11:04:43      11/06/2015 11:52:31        1503,97
+            D:\temp\vsphere6.1\VMware-... 29/06/2015 13:41:37      29/06/2015 13:39:12          868,5
+            D:\temp\vsphere6.1\VMware-... 29/06/2015 14:04:48      29/06/2015 14:03:34         867,63
+            D:\temp\vsphere6.1\vRealiz... 16/06/2015 11:03:56      15/06/2015 12:10:53         404,82
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:08:54      20/05/2015 11:56:10         348,38
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:09:31      20/05/2015 10:06:22         171,72
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:09:16      20/05/2015 10:05:25         119,95
+            D:\temp\vsphere6.1\VMware-... 17/06/2015 09:35:40      11/06/2015 11:23:47          35,82
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:09:20      20/05/2015 10:03:19          31,33
+    .EXAMPLE
+            Get-BigFiles -Path "D:\temp\vsphere6.1" -MinSize 1Gb
+
+            FullName                      Date Created             Date Modified            Size (MB)
+            --------                      ------------             -------------            ---------
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:07:33      20/05/2015 11:50:35         2613,7
+            D:\temp\vsphere6.1\vRealiz... 16/06/2015 11:04:43      11/06/2015 11:52:31        1503,97
+
+    .EXAMPLE
+            "D:\temp\vsphere6.1" | Get-BigFiles -MinSize 750Mb
+
+            FullName                      Date Created             Date Modified            Size (MB)
+            --------                      ------------             -------------            ---------
+            D:\temp\vsphere6.1\VMware-... 20/05/2015 12:07:33      20/05/2015 11:50:35         2613,7
+            D:\temp\vsphere6.1\vRealiz... 16/06/2015 11:04:43      11/06/2015 11:52:31        1503,97
+            D:\temp\vsphere6.1\VMware-... 29/06/2015 13:41:37      29/06/2015 13:39:12          868,5
+            D:\temp\vsphere6.1\VMware-... 29/06/2015 14:04:48      29/06/2015 14:03:34         867,63
+    .NOTES
+            NAME:     Get-BigFiles
+            AUTHOR:   ROULEAU Benjamin
+            LASTEDIT: 2015-07-07
+    .LINKS
+            http://katalykt.blogspot.fr/
+#>
+
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateScript({Test-Path -Path $_ -PathType Container})]
+        $Path,
+        
+        [ValidateScript({$_.GetType().Name -eq "Int32"})]
+        $MinSize=25Mb
+    )
+
+    BEGIN {
+        Write-Verbose -Message "[BEGIN] Scan began at: $((Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))"
+    }
+
+    PROCESS {
+        Write-Verbose -Message "[PROCESS] Scanning folder: $Path"
+
+        # Get the content of the given folder and fetch files which are higher than the given size threshold
+        Get-ChildItem -Path $Path -Recurse -ErrorAction "SilentlyContinue" |
+            Where-Object {$_.Length -ge $MinSize} |            Select-Object FullName, `                @{Name="Date Created";Expression={$_.CreationTime.ToString()}}, `
+                @{Name="Date Modified";Expression={$_.LastWriteTime.ToString()}}, `
+                @{Name="Size (MB)";Expression={[math]::Round($_.Length / 1Mb, 2)}} |
+            Sort-Object "Size (MB)" -Descending
+    }
+
+    END {
+        Write-Verbose -Message "[END] Scan ended at: $((Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))"
+    }
+}
---
+++ Windows/Set-SNMPProperties.ps1
@@ -0,0 +1,164 @@
+﻿Function Set-SNMPProperties {
+    #requires -version 3
+
+<#
+    .SYNOPSIS
+            This function will install SNMP if needed and configure the Permitted Managers along with
+            the desired communities in read-only via WSMan
+    .DESCRIPTION
+            This function will install SNMP if needed and configure the Permitted Managers along with
+            the desired communities in read-only via WSMan
+    .PARAMETER  Computer
+            Specifies which computer(s) will be processed
+    .PARAMETER  SNMPPermittedManager 
+            Specifies which Managers are allowed
+    .PARAMETER  SNMPCommunity
+            Specifies which Communities are allowed
+    .PARAMETER  SNMPClear
+            Specifies whether the existing Managers/Communities should be cleared or not
+    .EXAMPLE
+            Set-SNMPProperties -Computer "lab-fs001.katalykt.lan" `
+                -SNMPPermittedManager "lab-centreon001.katalykt.lan" `
+                -SNMPCommunity "katalyktRO"
+
+            PSComputerName           SNMPManagers                   SNMPCommunities 
+            --------------           ------------                   --------------- 
+            lab-fs001.katalykt.lan   lab-centreon001.katalykt.lan   katalyktRO
+    .EXAMPLE
+            Set-SNMPProperties -Computer "lab-fs001.katalykt.lan", "lab-fs002.katalykt.lan" `
+                -SNMPPermittedManager "lab-centreon001.katalykt.lan", "lab-centreon002.katalykt.lan" `
+                -SNMPCommunity "katalyktRO", "labRO"
+
+            PSComputerName           SNMPManagers                                         SNMPCommunities 
+            --------------           ------------                                         --------------- 
+            lab-fs001.katalykt.lan   {lab-centreon001.katalykt.lan, lab-centreon002...}   {katalyktRO, labRO}
+            lab-fs002.katalykt.lan   {lab-centreon001.katalykt.lan, lab-centreon002...}   {katalyktRO, labRO}
+    .EXAMPLE
+            # Assuming the CSV contains one column with the title "Computer"
+            Import-Csv -Path D:\Scripts\PSv3\Set-SNMPProperties.csv | ForEach-Object {$_.Computer} | `
+                Set-SNMPProperties -SNMPPermittedManager "lab-centreon001.katalykt.lan" `
+                    -SNMPCommunity "katalyktRO" `                    -SNMPClear
+
+            PSComputerName           SNMPManagers                   SNMPCommunities 
+            --------------           ------------                   --------------- 
+            lab-fs001.katalykt.lan   lab-centreon001.katalykt.lan   katalyktRO
+            lab-fs002.katalykt.lan   lab-centreon001.katalykt.lan   katalyktRO
+            lab-fs003.katalykt.lan   lab-centreon001.katalykt.lan   katalyktRO
+            lab-fs004.katalykt.lan   lab-centreon001.katalykt.lan   katalyktRO
+    .NOTES
+            NAME:     Set-SNMPProperties
+            AUTHOR:   ROULEAU Benjamin
+            LASTEDIT: 2016-11-17
+    .LINKS
+            http://katalykt.blogspot.fr/
+#>
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        $Computer,
+        
+        $SNMPPermittedManager,
+
+        $SNMPCommunity,
+
+        [switch]$SNMPClear
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+        $Computer | ForEach-Object {
+            Try {
+                Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Processing computer [$_]"
+
+                If (Test-WSMan -ComputerName $_ -ErrorAction SilentlyContinue) {
+                    Invoke-Command -ComputerName $_ -ErrorAction Stop -ErrorVariable ErrInvoke -ScriptBlock {
+                        $VerbosePreference = $Using:VerbosePreference
+                        
+                        # Install the SNMP Service with the WMI provider if needed
+                        If ((Get-WindowsFeature -Name "SNMP-Service").Installed -eq "True") {
+                            Write-Verbose -Message "[PROCESS - Set-SNMPProperties] The SNMP-Service Windows Feature is already installed"
+                        } Else {
+                            Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Installing the SNMP-Service Windows Feature"
+                            
+                            Get-WindowsFeature -Name "SNMP-Service" | Add-WindowsFeature -IncludeAllSubFeature -IncludeManagementTools
+                        }
+
+                        # Clear the existing managers/communities if specified
+                        If ($SNMPClear) {
+                            Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Clearing up the existing Managers and Communities"
+
+                            "PermittedManagers", "ValidCommunities" | ForEach-Object {
+                                $Subkey = $_
+                                (Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\$Subkey").GetValueNames() |  ForEach-Object {
+                                    Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Clearing up existing registry record: HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\$Subkey\$_"
+
+                                    Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\$Subkey" -Name $_
+                                }
+                            }
+                        }
+
+                        # Check if the given permitted managers are defined or not
+                        Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Checking up Permitted Managers..."
+
+                        $LocalManagers = $using:SNMPPermittedManager
+                        If ($LocalManagers) {
+                            $PermittedManagers = Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers"
+                            $Index = ($PermittedManagers.ValueCount)+1
+    
+                            $PermittedManagers.GetValueNames() | ForEach-Object {
+                                # If this manager is located within the manager array, we remove it from it
+                                $Item = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers" -Name $_).$_
+                            
+                                If ($Item -in $LocalManagers) {
+                                    Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Skipping Manager $Item since it's already present"
+                                    $LocalManagers = $LocalManagers | Where-Object {$_ -ne $Item}
+                                }
+                            }
+
+                            # Add the missing managers if needed
+                            $LocalManagers | ForEach-Object {
+                                Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Adding Manager $_ in the Permitted list with Index $Index"
+
+                                New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers" -Name $Index -Value $_
+                                $Index++
+                            }
+                        }
+
+                        # Check if the community exists, if not we add it
+                        Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Checking up the communities..."
+
+                        $using:SNMPCommunity | ForEach-Object {
+                            If (-not(Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities" -Name $_ -ErrorAction SilentlyContinue)) {
+                                Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Adding Community $_"
+
+                                New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities" -Name $_ -Value 4 -PropertyType DWORD
+                            } Else {
+                                Write-Verbose -Message "[PROCESS - Set-SNMPProperties] Skipping Community $_ since it's already present"
+                            }
+                        }
+
+                        # Return the new SNMP settings for this host
+                        New-Object -TypeName PSObject -Property @{
+                            SNMPCommunities = (Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities").GetValueNames()
+                            SNMPManagers = (Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers").GetValueNames() | ForEach-Object {(Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers" -Name $_).$_}
+                        }
+                    }
+                } Else {
+                    Write-Warning -Message "[PROCESS - Set-SNMPProperties] Computer [$_] could not be contacted via WSMan"
+                }
+            } Catch {
+                If ($ErrInvoke) { Write-Warning -Message ("[PROCESS - Set-SNMPProperties] Invoke-Command failed for computer [{0}] with error {1}" -f $Error[0].OriginInfo, $Error[0].Exception.Message) }
+            }
+        } | Select PSComputerName, SNMPManagers, SNMPCommunities
+    }
+
+    END {
+
+    }
+}
