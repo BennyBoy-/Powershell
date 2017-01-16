7290ca3328a8ae15de241aaf3c5ff229349ba020
Benny <benny_boy66@hotmail.fr>
2017-01-16 09:52:19 +0100

    Fixed Markup

--- Windows/Set-SNMPProperties.ps1
+++ Windows/Set-SNMPProperties.ps1
@@ -37,7 +37,7 @@
             # Assuming the CSV contains one column with the title "Computer"
             Import-Csv -Path D:\Scripts\PSv3\Set-SNMPProperties.csv | ForEach-Object {$_.Computer} | `
                 Set-SNMPProperties -SNMPPermittedManager "lab-centreon001.katalykt.lan" `
-                    -SNMPCommunity "katalyktRO" ` 
+                    -SNMPCommunity "katalyktRO" `
                     -SNMPClear
 
             PSComputerName           SNMPManagers                   SNMPCommunities 
