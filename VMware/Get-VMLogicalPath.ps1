b833bfe8fa0c2b9cdbe1dfe1b87c3b3bbb630232
Benny <benny_boy66@hotmail.fr>
2017-01-16 09:42:44 +0100

    Added VMware scripts

---
+++ VMware/Get-VMLogicalPath.ps1
@@ -0,0 +1,78 @@
+﻿#requires -version 2
+
+Function Get-VMLogicalPath {
+<#
+    .SYNOPSIS
+            This function will return a VM's logical path within vCenter
+    .DESCRIPTION
+            This function will return a VM's logical path within vCenter with or without the VM's name
+    .PARAMETER  VM
+            Specifies a vCenter VM
+    .EXAMPLE
+            Get-VMLogicalPath -VM (Get-VM -Name "Master")
+
+            Name                 VMPath                                        ParentPath                                           
+            ----                 ------                                        ----------                                           
+            Master               /KatalyktDC/vm/PoolFolder/Master              /KatalyktDC/vm/PoolFolder  
+    .EXAMPLE
+            $VMArray | Get-VMLogicalPath
+
+            Name                 VMPath                                        ParentPath                                           
+            ----                 ------                                        ----------                                           
+            Master               /KatalyktDC/vm/PoolFolder/Master              /KatalyktDC/vm/PoolFolder  
+            MasterTraining       /KatalyktDC/vm/PoolFolder/MasterTraining      /KatalyktDC/vm/PoolFolder  
+    .NOTES
+            NAME:     Get-VMLogicalPath
+            AUTHOR:   ROULEAU Benjamin
+            LASTEDIT: 2014-02-05
+    .LINKS
+            http://katalykt.blogspot.fr/
+#>
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [VMware.VimAutomation.ViCore.Impl.V1.Inventory.VirtualMachineImpl]$VM
+    )
+
+    BEGIN {
+        
+    }
+
+    PROCESS {
+        $VM | ForEach-Object {
+            TRY {
+                Write-Verbose -Message ("[PROCESS - Get-VMLogicalPath] Retrieving path for VM {0}" -f $_.Name)
+
+                # We keep retrieving the parent's folder up to the Datacenter root
+                $VMo = $_.Folder
+                $Path = ""
+                WHILE ($VMo) {
+                    $Path = "/" + $VMo.Name + $Path
+                    $VMo = $VMo.Parent
+                }
+
+                # Compose the VM Path
+                $VMPath = $Path + "/" + $_.Name
+                
+                Write-Verbose -Message ("[PROCESS - Get-VMLogicalPath] Retrieved path {0} for VM {1}" -f $VMPath, $_.Name)
+
+                # Return the desired properties in an object
+                New-Object -TypeName PSObject -Property @{
+                    Name = $_.Name
+                    ParentPath = $Path
+                    VMPath = $VMPath
+                }
+                
+            } CATCH {
+                Write-Warning -Message ("[PROCESS - Get-VMLogicalPath] Something went wrong: {0}" -f $Error[0].Exception.Message)
+            }
+        }
+    }
+
+    END {
+
+    }
+}
---
+++ VMware/Get-VMSnapShotPath.ps1
@@ -0,0 +1,64 @@
+﻿#Requires -Version 3.0
+
+Function Get-VMSnapshotPath {
+<#
+    .SYNOPSIS
+            This function will return a VM's Snapshot path
+    .DESCRIPTION
+            This function will return a VM's Snapshot path
+    .PARAMETER  Snapshot
+            Specifies a VM Snapshot
+    .EXAMPLE
+            Get-Snapshot -VM (Get-VM "Master") -Name "BaseSnapshot" | Get-VMSnapshotPath
+
+            /BaseSnapshot
+    .EXAMPLE
+            Get-Snapshot -VM (Get-VM "Master") -Name "SecondSnapshot" | Get-VMSnapshotPath
+
+            /BaseSnapshot/SecondSnapshot
+    .EXAMPLE
+            (Get-VM "Master"), (Get-VM "MasterVDI") | Get-Snapshot | Get-VMSnapshotPath
+
+            /BaseSnapshot
+            /BaseSnapshot/SecondSnapshot
+            /VDIBaseSnapshot
+    .NOTES
+            NAME:     Get-VMLogicalPath
+            AUTHOR:   ROULEAU Benjamin
+            LASTEDIT: 2015-02-05
+    .LINKS
+            http://katalykt.blogspot.fr/
+#>
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [VMware.VimAutomation.ViCore.Impl.V1.VM.SnapshotImpl]$Snapshot
+    )
+
+    BEGIN {
+        
+    }
+
+    PROCESS {
+        $Snapshot | ForEach-Object {
+            Write-Verbose -Message ("[PROCESS - Get-VMSnapshotPath] Retrieving the path of Snapshot: '{0}' on VM : '{1}'" -f $_.Name, $_.VM.Name)
+
+            # We retrieve the Snapshot path up the the root of the VM
+            $VMo = $_
+            $Path = ""
+            WHILE ($VMo) {
+                $Path = "/" + $VMo.Name + $Path
+                $VMo = $VMo.ParentSnapshot
+            }
+
+            $Path
+        }
+    }
+
+    END {
+
+    }
+}
